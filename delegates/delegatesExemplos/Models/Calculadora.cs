fd7583eeae33596ca609ea9d70efebb2e1e71c60
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-01-05 21:33:19 -0400

    Delegates em C#
    Praticando com delegates

---
+++ delegates/delegatesExemplos/Models/Calculadora.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace delegatesExemplos.Models
+{
+    public class Calculadora
+    {
+        public static float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ delegates/delegatesExemplos/Program.cs
@@ -0,0 +1,20 @@
+﻿using delegatesExemplos.Models;
+using System;
+
+
+namespace delegatesExemplos
+{
+
+    class Program
+    {
+        public delegate float operacao(float n1, float n2);
+        static void Main(string[] args)
+        {
+            operacao op = Calculadora.Somar;
+            var resultado = op.Invoke(2, 3);
+            Console.WriteLine(resultado);
+        }
+    }
+}
+
+
